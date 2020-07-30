46011bdff33b1671e8e0f76a70bbec871558119b
haoshuimofu <279759300@qq.com>
2020-07-30 09:53:25 +0800

    rocketmq init

---
+++ rocketmq/src/main/java/com/demo/components/rocketmq/SyncProducer.java
@@ -0,0 +1,38 @@
+package com.demo.components.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @author wude
+ * @date 2020/7/29 15:50
+ */
+public class SyncProducer {
+
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("RocketMQTest");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+//        producer.createTopic("TopicTest", "TopicTest", 1);
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+}
