4d7cd606b10e788b1a34c5d1d09ce3ad5a08b7df
haoshuimofu <279759300@qq.com>
2020-08-07 14:38:38 +0800

    okhttpclient

---
+++ general-tests/src/main/java/com/demo/okhttp/OkHttpClientTest.java
@@ -0,0 +1,35 @@
+package com.demo.okhttp;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @author wude
+ * @date 2020/8/7 14:05
+ */
+public class OkHttpClientTest {
+
+    public static void main(String[] args) throws Exception {
+        // get from a url
+        getAURL();
+    }
+
+    public static void getAURL() throws Exception {
+        String url = "http://www.baidu.com";
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.err.println(response.body().string());
+        }
+    }
+
+    public static void postToAServer() {
+
+    }
+}
