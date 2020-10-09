32cd0b1916db468ed407afa1011ee4f556490ee4
haoshuimofu <279759300@qq.com>
2020-10-09 10:39:52 +0800

    JVM OOM异常测试

---
+++ general-tests/src/main/java/com/demo/jvm/OOMTest.java
@@ -0,0 +1,24 @@
+package com.demo.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wude
+ * @date 2020/10/8 14:37
+ */
+public class OOMTest {
+
+    // -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+HeapDumpOnOutOfMemoryError
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+    static class OOMObject {
+
+    }
+
+}
