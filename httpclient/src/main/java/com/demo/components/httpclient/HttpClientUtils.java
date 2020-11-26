0eeb3090c1b94e345a28bc25c2739a346015c4d8
haoshuimofu <279759300@qq.com>
2020-11-26 11:47:38 +0800

    commit

--- httpclient/src/main/java/com/demo/components/httpclient/HttpClientUtils.java
+++ httpclient/src/main/java/com/demo/components/httpclient/HttpClientUtils.java
@@ -3,15 +3,11 @@ package com.demo.components.httpclient;
 import com.alibaba.fastjson.JSON;
 import com.demo.components.httpclient.exception.HttpException;
 import com.demo.components.httpclient.exception.HttpStatusException;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -50,11 +46,15 @@ public class HttpClientUtils {
     public static String get(String url, Map<String, String> headers, int connectTimeout, int socketTimeout) throws Exception {
         CloseableHttpClient httpClient = HttpClients.custom().build();
         HttpGet httpGet = new HttpGet(url);
-        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
-                .setConnectTimeout(connectTimeout > 0 ? connectTimeout : -1)
-                .setSocketTimeout(socketTimeout > 0 ? socketTimeout : -1);
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
+        if (connectTimeout > 0) {
+            requestConfigBuilder.setConnectTimeout(connectTimeout);
+        }
+        if (socketTimeout > 0) {
+            requestConfigBuilder.setSocketTimeout(socketTimeout);
+        }
         httpGet.setConfig(requestConfigBuilder.build());
-        HttpHelper.addHeaders(httpGet, headers);
+        HttpClientUtils.addHeaders(httpGet, headers);
         return HttpClientUtils.executeHttpRequest(httpClient, httpGet);
     }
 
@@ -67,14 +67,18 @@ public class HttpClientUtils {
                               int connectTimeout, int socketTimeout, String charset) throws Exception {
         CloseableHttpClient httpclient = HttpClients.custom().build();
         HttpPost httpPost = new HttpPost(url);
-        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
-                .setConnectTimeout(connectTimeout > 0 ? connectTimeout : -1)
-                .setSocketTimeout(socketTimeout > 0 ? socketTimeout : -1);
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
+        if (connectTimeout > 0) {
+            requestConfigBuilder.setConnectTimeout(connectTimeout);
+        }
+        if (socketTimeout > 0) {
+            requestConfigBuilder.setSocketTimeout(socketTimeout);
+        }
         httpPost.setConfig(requestConfigBuilder.build());
-        HttpHelper.addHeaders(httpPost, headers);
+        HttpClientUtils.addHeaders(httpPost, headers);
         if (params != null) {
             httpPost.setEntity((charset == null || charset.trim().isEmpty()) ?
-                    new StringEntity(params) : new StringEntity(params, charset));
+                    new StringEntity(params) : new StringEntity(params, charset.trim()));
         }
         return HttpClientUtils.executeHttpRequest(httpclient, httpPost);
     }
@@ -83,18 +87,28 @@ public class HttpClientUtils {
                                   int connectTimeout, int socketTimeout, String charset) throws Exception {
         CloseableHttpClient httpclient = HttpClients.custom().build();
         HttpPost httpPost = new HttpPost(url);
-        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
-                .setConnectTimeout(connectTimeout > 0 ? connectTimeout : -1)
-                .setSocketTimeout(socketTimeout > 0 ? socketTimeout : -1);
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
+        if (connectTimeout > 0) {
+            requestConfigBuilder.setConnectTimeout(connectTimeout);
+        }
+        if (socketTimeout > 0) {
+            requestConfigBuilder.setSocketTimeout(socketTimeout);
+        }
         httpPost.setConfig(requestConfigBuilder.build());
-        HttpHelper.addHeaders(httpPost, headers);
+        HttpClientUtils.addHeaders(httpPost, headers);
+        if (charset != null) {
+            charset = charset.trim();
+            if (charset.isEmpty()) {
+                charset = null;
+            }
+        }
         if (params != null && params.size() > 0) {
             List<NameValuePair> nameValuePairs = new ArrayList<>(params.size());
             for (Map.Entry<String, Object> entry : params.entrySet()) {
-                nameValuePairs.add(new BasicNameValuePair(entry.getKey(),
-                        Optional.ofNullable(entry.getValue()).orElse("").toString()));
+                nameValuePairs.add(new BasicNameValuePair(
+                        entry.getKey(), Optional.ofNullable(entry.getValue()).orElse("").toString()));
             }
-            if (StringUtils.isNotBlank(charset)) {
+            if (charset != null) {
                 httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, charset));
             } else {
                 httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
@@ -103,13 +117,20 @@ public class HttpClientUtils {
         return HttpClientUtils.executeHttpRequest(httpclient, httpPost);
     }
 
+    /**
+     * 执行HTTP请求
+     *
+     * @param httpClient
+     * @param httpRequest
+     * @return
+     * @throws Exception
+     */
     public static String executeHttpRequest(CloseableHttpClient httpClient, HttpUriRequest httpRequest) throws Exception {
         CloseableHttpResponse response = null;
-        boolean responseClose = false;
         try {
             response = httpClient.execute(httpRequest);
             if (response == null) {
-                throw new HttpException("Http response is null");
+                throw new HttpException("Http response is null.");
             }
             StatusLine statusLine = response.getStatusLine();
             if (statusLine.getStatusCode() != HttpStatus.OK.getValue()) {
@@ -117,23 +138,29 @@ public class HttpClientUtils {
                         httpRequest.getMethod(), statusLine.getStatusCode(), statusLine.getReasonPhrase());
                 throw new HttpStatusException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
             }
-            String responseStr = EntityUtils.toString(response.getEntity());
-            responseClose = true;
-            return responseStr;
+            return EntityUtils.toString(response.getEntity());
         } finally {
-            if (response != null && !responseClose) {
+            if (response != null) {
                 try {
                     response.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            // 使用连接池, 连接交给ConnectionManager管理, 如果httpCleint.close会直接关闭连接, 重复使用时会导致报错
+            // 使用连接池, 连接交给ConnectionManager管理, 如果httpClient.close会直接关闭连接, 重复使用时会导致报错
             // Connection pool shut down
             try {
                 httpClient.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    public static void addHeaders(HttpRequestBase httpRequestBase, Map<String, String> headers) {
+        if (httpRequestBase != null && headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+                httpRequestBase.addHeader(headerEntry.getKey(), headerEntry.getValue());
             }
         }
     }
