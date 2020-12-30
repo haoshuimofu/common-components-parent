68ba8c2bf2d24e3f65366c859004045bfa679014
haoshuimofu <279759300@qq.com>
2020-12-30 10:46:31 +0800

    删除无用代码

---
+++ spring-data-redis-cluster-starter/src/main/java/com/demo/components/redission/Redission.java
@@ -0,0 +1,10 @@
+package com.demo.components.redission;
+
+/**
+ * {@link <a>https://github.com/redisson/redisson/wiki/%E7%9B%AE%E5%BD%95</a>}
+ *
+ * @author wude
+ * @date 2020/12/30 10:37
+ */
+public class Redission {
+}
