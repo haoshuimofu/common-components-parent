42f2316ad2af57ac913705569e6326202f5c91dd
haoshuimofu <279759300@qq.com>
2021-06-16 11:15:22 +0800

    IP解析工具

--- general-tests/src/main/java/com/demo/algorithm/IPUtil.java
+++ general-tests/src/main/java/com/demo/algorithm/IPUtil.java
@@ -1,5 +1,8 @@
 package com.demo.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wude
  * @date 2021/6/10 11:03
@@ -8,53 +11,53 @@ public class IPUtil {
 
     public static void main(String[] args) {
 
+        List<String> ipList = new ArrayList<>();
+
         String ip = "2551551893";
-        int len = ip.length();
+        int round = 0;
+        while (true) {
+            int len = ip.length();
 
-        int round = 3;
+            for (int i = 1; i <= 3; i++) {
 
-        for (int i = 1; i <= 3 ; i++) {
+                for (int j = i + 1; j <= i + 3; j++) {
 
-            for (int j = i+1; j <=i+3 ; j++) {
+                    for (int x = j + 1; x <= j + 3; x++) {
 
-                for (int x = j+1; x <=j+3 ; x++) {
+                        for (int y = x + 1; y <= x + 3; y++) {
 
-                    for (int y = x+1; y <=x+3 ; y++) {
-
-                        if (y != len) {
-                            continue;
+                            if (y > len) {
+                                continue;
+                            }
+                            int round1 = Integer.valueOf(ip.substring(0, i));
+                            int round2 = Integer.valueOf(ip.substring(i, j));
+                            int round3 = Integer.valueOf(ip.substring(j, x));
+                            int round4 = Integer.valueOf(ip.substring(x, y));
+                            if (round1 <= 0 || round1 > 255) {
+                                continue;
+                            }
+                            if (round2 <= 0 || round2 > 255) {
+                                continue;
+                            }
+                            if (round3 <= 0 || round3 > 255) {
+                                continue;
+                            }
+                            if (round4 <= 0 || round4 > 255) {
+                                continue;
+                            }
+                            ipList.add(round1 + "." + round2 + "." + round3 + "." + round4);
                         }
-                        int round1 = Integer.valueOf(ip.substring(0, i));
-                        int round2 = Integer.valueOf(ip.substring(i, j));
-                        int round3 = Integer.valueOf(ip.substring(j, x));
-                        int round4 = Integer.valueOf(ip.substring(x, y));
-                        if (round1 <=0 || round1 > 255) {
-                            continue;
-                        }
-                        if (round2 <=0 || round2 > 255) {
-                            continue;
-                        }
-                        if (round3 <=0 || round3 > 255) {
-                            continue;
-                        }
-                        if (round4 <=0 || round4 > 255) {
-                            continue;
-                        }
-
-                        System.err.println(round1 + "." + round2 + "." + round3+ "." + round4);
-
-
                     }
-
                 }
-
-
             }
-
-
+            ip = ip.substring(1);
+            if (ip.length() < 4) {
+                break;
+            }
         }
-
-
+        for (String ipStr : ipList) {
+            System.err.println(ipStr);
+        }
 
     }
 
