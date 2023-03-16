147081102e7658fcf77464366840d5af908ed1fc
德夏 <dewu.de@alibaba-inc.com>
2023-03-16 10:34:40 +0800

    okhttp

--- general-tests/src/main/java/test/threadpool/ScheduleThreadPoolTest.java
+++ general-tests/src/main/java/test/threadpool/ScheduleThreadPoolTest.java
@@ -12,7 +12,6 @@ import java.util.concurrent.TimeUnit;
  * @create 2018-12-13 13:00
  */
 public class ScheduleThreadPoolTest {
-    static int count = 0;
 
     public static void main(String[] args) {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
@@ -21,11 +20,11 @@ public class ScheduleThreadPoolTest {
 
         // 初始化延迟10S后启动, 以后每5S启动一次, 这个时候就不能shutdown停止线程池接收新任务了, 而且如果task本身执行时长超过period, 那么本次task执行完毕以后下次task立即执行
         // --------> 延迟时间计算始于上个task的《《启动》》时间，而如果task执行时间大于间隔时间，那么延迟失效
-        // executorService.scheduleAtFixedRate(new Task(), 10, 5, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(new Task(), 10, 5, TimeUnit.SECONDS);
 
         // 初始化延迟10S后启动, 以后每5S启动一次, 这个时候就不能shutdown停止线程池接收新任务了, 而且如果task本身执行时长超过period, 那么本次task执行完毕以后还是会等待延迟时间后下次task才执行
         // --------> 延迟时间计算始于上个task的《《结束》》时间，而如果task执行时间大于间隔时间无影响
-        executorService.scheduleWithFixedDelay(new Task(), 10, 5, TimeUnit.SECONDS);
+//        executorService.scheduleWithFixedDelay(new Task(), 10, 5, TimeUnit.SECONDS);
 
 //        executorService.shutdown();
 //        try {
@@ -38,25 +37,19 @@ public class ScheduleThreadPoolTest {
     }
 
 
-    private static class Task implements Runnable {
+    static class Task implements Runnable {
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         @Override
         public void run() {
-//            long startTime = System.currentTimeMillis();
-//            System.out.println("start at: " + startTime);
-//
-//            try {
-//                Thread.sleep(10000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//
-//            long endTime = System.currentTimeMillis();
-//            System.out.println("end at: " + endTime);
-//            System.out.println("时间差: " + (endTime - startTime));
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("now is " + sdf.format(new Date()));
         }
+
     }
 }
