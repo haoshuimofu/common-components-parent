f521cba1a44eb9ce74ad420cc7448bbd0c67c581
德夏 <dewu.de@alibaba-inc.com>
2023-03-17 16:34:41 +0800

    sharding-jdbc test

--- shardingsphere/src/main/java/com/demo/components/shardingjdbc/service/OrderService.java
+++ shardingsphere/src/main/java/com/demo/components/shardingjdbc/service/OrderService.java
@@ -11,10 +11,11 @@ import java.util.Date;
 /**
  * DB操作和事务测试
  * <p>问题1: 系统自动创建TransactionManager, mvn tree显示spring-tx包是mybatis-spring-boot-starter引入的</p>
+ * <p>启动时debug类: DataSourceTransactionManager, 最终会调到其无参构造方法，往前追随到{@link org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration}</p>
  */
 @Slf4j
 @Service
-@Transactional(rollbackFor = IllegalArgumentException.class) // 默认没有事务，异常就回滚
+@Transactional(rollbackFor = IllegalArgumentException.class)
 public class OrderService {
 
     private final OrderModelDao orderModelDao;
@@ -31,6 +32,15 @@ public class OrderService {
         }
         order.setCreateTime(new Date());
         order.setUpdateTime(order.getCreateTime());
+        orderModelDao.insert(order);
+
+        String orderId1 = String.valueOf(Integer.parseInt(order.getOrderId()) + 1);
+        OrderModel existsOrder1 = orderModelDao.selectById(orderId1);
+        if (existsOrder1 != null) {
+            log.info("订单已存在: orderId=[{}]", order.getOrderId());
+            orderModelDao.deleteById(orderId1);
+        }
+        order.setOrderId(orderId1);
         orderModelDao.insert(order);
 
         int testNum = 1 / 0;
