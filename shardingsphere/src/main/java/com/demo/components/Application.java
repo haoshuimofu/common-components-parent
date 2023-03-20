abe5d9f3f69321ef3a74fcfcf6785eefe7b7377f
德夏 <dewu.de@alibaba-inc.com>
2023-03-20 10:26:54 +0800

    sharding-jdbc transaction

--- shardingsphere/src/main/java/com/demo/components/Application.java
+++ shardingsphere/src/main/java/com/demo/components/Application.java
@@ -3,20 +3,21 @@ package com.demo.components;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.TransactionManager;
 
 /**
  * Hello world!
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceTransactionManagerAutoConfiguration.class})
 @MapperScan("com.demo.components.shardingjdbc.dao")
 public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        TransactionManager tm = ctx.getBean(TransactionManager.class);
-        System.out.println(tm == null);
+//        TransactionManager tm = ctx.getBean(TransactionManager.class);
+//        System.out.println(tm == null);
 
     }
 }
