14f38a0b95ef20fc5a8edb30e51b03a02988b564
wd <279759300@qq.com>
2023-07-03 20:40:54 +0800

    leetcode 20230701

--- general-tests/src/main/java/leetcode/Test_20230703.java
+++ general-tests/src/main/java/leetcode/Test_20230703.java
@@ -1,5 +1,12 @@
 package leetcode;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author dewu.de
  * @date 2023-07-03 1:33 下午
@@ -11,6 +18,9 @@ public class Test_20230703 {
     public static void main(String[] args) {
         TreeNode root = TreeNode_Init.init();
         System.out.println(isValidBST(root));
+
+        BSTIterator iterator = new BSTIterator(root);
+        System.out.println(JSON.toJSONString(iterator));
     }
 
     /**
@@ -27,16 +37,45 @@ public class Test_20230703 {
         if (!isValidBST(root.left)) {
             return false;
         }
-        if (prev != null) {
-            if (root.val >= prev.val) {
-                return false;
-            }
-        } else {
-            prev = root;
+        if (prev != null && root.val <= prev.val) {
+            return false;
         }
+        prev = root;
         return isValidBST(root.right);
-
     }
 
+    // BST迭代器实现
+    @Getter
+    @Setter
+    static class BSTIterator {
+
+        private List<Integer> iterator;
+        private int index = -1;
+
+        public BSTIterator(TreeNode root) {
+            iterator = new ArrayList<>();
+            inorder(root);
+        }
+
+        public int next() {
+            if (hasNext()) {
+                return iterator.get(++index);
+            }
+            return 0;
+        }
+
+        public boolean hasNext() {
+            return iterator.size() > 0 && index < iterator.size() - 1;
+        }
+
+        private void inorder(TreeNode node) {
+            if (node == null) {
+                return;
+            }
+            inorder(node.left);
+            iterator.add(node.val);
+            inorder(node.right);
+        }
+    }
 
 }
