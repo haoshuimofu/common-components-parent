50d2127b503fa8ffc426ce3671167fd77783555c
德夏 <dewu.de@alibaba-inc.com>
2023-07-10 15:46:23 +0800

    最小路径和

--- general-tests/src/main/java/leetcode/Test_20230710.java
+++ general-tests/src/main/java/leetcode/Test_20230710.java
@@ -11,6 +11,7 @@ public class Test_20230710 {
 
 
     public static void main(String[] args) {
+        // 杨辉三角
         List<List<Integer>> result = generate(5);
         for (List<Integer> integers : result) {
             for (Integer integer : integers) {
@@ -18,6 +19,13 @@ public class Test_20230710 {
             }
             System.out.println();
         }
+        // 最小路径和
+        int[][] grid = new int[3][3];
+        grid[0] = new int[]{1, 3, 1};
+        grid[1] = new int[]{1, 5, 1};
+        grid[2] = new int[]{4, 2, 1};
+        System.out.println(minPathSum(grid));
+
     }
 
     /**
@@ -53,5 +61,37 @@ public class Test_20230710 {
             result.add(list);
         }
         return result;
+    }
+
+    /**
+     * 最小路径和
+     * m*n个格子
+     * 1 3 1
+     * 1 5 1
+     * 4 2 1
+     * <p>
+     * dp[m,n] = min(dp[m][n-1], dp[m-1][n]) + grid[m][n]
+     *
+     * @param grid
+     * @return
+     */
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int value = grid[i][j];
+                if (i >= 1 && j >= 1) {
+                    value += Math.min(dp[i][j - 1], dp[i - 1][j]);
+                } else if (i >= 1) {
+                    value += dp[i - 1][j];
+                } else if (j >= 1) {
+                    value += dp[i][j - 1];
+                }
+                dp[i][j] = value;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
