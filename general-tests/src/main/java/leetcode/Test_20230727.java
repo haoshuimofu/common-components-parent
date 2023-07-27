b220c690c5d54b304ecb5a2bd987849c79519d35
德夏 <dewu.de@alibaba-inc.com>
2023-07-27 15:11:05 +0800

    24. 寻找数组的中心下标

--- general-tests/src/main/java/leetcode/Test_20230727.java
+++ general-tests/src/main/java/leetcode/Test_20230727.java
@@ -14,6 +14,11 @@ public class Test_20230727 {
         mat[1] = new int[]{4, 5, 6};
         mat[2] = new int[]{1, 8, 9};
         System.out.println(JSON.toJSONString(findDiagonalOrder(mat)));
+        System.out.println("/////////");
+        int[] nums = new int[]{1, 7, 3, 6, 5, 6};
+//        System.out.println(pivotIndex(nums));
+        nums = new int[]{-1, -1, -1, -1, 0, 0};
+        System.out.println(pivotIndex(nums));
     }
 
 
@@ -69,5 +74,63 @@ public class Test_20230727 {
             }
         }
         return res;
+    }
+
+    /**
+     * 724. 寻找数组的中心下标
+     * <p>
+     * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+     * <p>
+     * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+     * <p>
+     * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+     * <p>
+     * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+     * </p>
+     *
+     * @param nums
+     * @return
+     */
+    public static int pivotIndex(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        } else if (nums.length == 1) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        int sum = 0;
+        int[] lowSum = new int[nums.length];
+        while (low <= high) {
+            if (low == 0) {
+                lowSum[low] = nums[low];
+            } else {
+                lowSum[low] = lowSum[low - 1] + nums[low];
+            }
+            sum += nums[low];
+            if (low != high) {
+                sum += nums[high];
+            }
+            low++;
+            high--;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 注意, 前面while条件不成立, low++后才不成立, 这里需要>=
+            if (i >= low) {
+                lowSum[i] = lowSum[i - 1] + nums[i];
+            }
+            if (i == 0) {
+                if (sum - nums[i] == 0) {
+                    return i;
+                }
+            } else if (i == nums.length - 1) {
+                if (lowSum[i - 1] == 0) {
+                    return i;
+                }
+            } else if (lowSum[i - 1] == sum - nums[i] - lowSum[i - 1]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
