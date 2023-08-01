15797526bec20fff7e892f81187a6e1c84622d3e
wd <279759300@qq.com>
2023-08-01 22:35:37 +0800

    我能想到的最优解-接雨水

--- general-tests/src/main/java/leetcode/Test_20230704.java
+++ general-tests/src/main/java/leetcode/Test_20230704.java
@@ -27,6 +27,7 @@ public class Test_20230704 {
     public static void main(String[] args) {
         int[] nums = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println("雨水=" + trap(nums));
+        System.out.println(trap2(nums));
     }
 
     /**
@@ -78,6 +79,58 @@ public class Test_20230704 {
                 slow = closeMin;
             } else {
                 break;
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 接雨水 - 我能想到的最优解
+     *
+     * @param height
+     * @return
+     */
+    public static int trap2(int[] height) {
+        // 数组长度小于3根本盛不了水
+        if (height == null || height.length <= 2) {
+            return 0;
+        }
+        int sum = 0;
+        int left = 0;
+        int right = 1;
+        while (left < right && right < height.length) {
+            if (height[right] >= height[left]) {
+                if (right - left > 1) {
+                    // 计算雨水
+                    for (int i = left + 1; i < right; i++) {
+                        sum += (height[left] - height[i]);
+                    }
+                }
+                left = right;
+                right = left + 1;
+            } else {
+                right++;
+            }
+        }
+        if (right >= height.length) {
+            // 说明left右边不存在right满足height[left] <= height[right]
+            // 那就把反方向计算
+            int index = left;
+            right = height.length - 1;
+            left = right - 1;
+            while (left < right && left >= index) {
+                if (height[left] >= height[right]) {
+                    if (right - left > 1) {
+                        // 计算雨水
+                        for (int i = right - 1; i > left; i--) {
+                            sum += (height[right] - height[i]);
+                        }
+                    }
+                    right = left;
+                    left = right - 1;
+                } else {
+                    left--;
+                }
             }
         }
         return sum;
