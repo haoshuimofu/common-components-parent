1fa8958f1544de9a951cc062c367c638a1b0841d
德夏 <dewu.de@alibaba-inc.com>
2023-08-03 18:56:27 +0800

    46.

--- general-tests/src/main/java/leetcode/Test_20230802.java
+++ general-tests/src/main/java/leetcode/Test_20230802.java
@@ -3,6 +3,7 @@ package leetcode;
 import com.alibaba.fastjson.JSON;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,6 +15,12 @@ public class Test_20230802 {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
         List<List<Integer>> res = permute(nums);
+        for (List<Integer> list : res) {
+            System.out.println(JSON.toJSONString(list));
+        }
+
+        nums = new int[]{1,1,2};
+        res = permuteUnique(nums);
         for (List<Integer> list : res) {
             System.out.println(JSON.toJSONString(list));
         }
@@ -41,6 +48,33 @@ public class Test_20230802 {
         }
         for (int i = 0; i < nums.length; i++) {
             if (visit[i] == false) {
+                path.add(nums[i]);
+                visit[i] = true;
+                System.out.println("dfs递归之前路径: " + JSON.toJSONString(path));
+                dfs(nums, count + 1, visit, path, permutes);
+
+                visit[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] visit = new boolean[nums.length];
+        List<List<Integer>> permutes = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs2(nums, 0, visit, new ArrayList<>(), permutes);
+        return permutes;
+    }
+
+    public static void dfs2(int[] nums, int count, boolean[] visit, List<Integer> path, List<List<Integer>> permutes) {
+        if (count == nums.length) {
+            // 当前path长度和nums长度一致, 说明是一个完成的组合了, path拷贝存放到结果集中
+            permutes.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visit[i] == false && (i == 0 || nums[i] != nums[i - 1])) {
                 path.add(nums[i]);
                 visit[i] = true;
                 System.out.println("dfs递归之前路径: " + JSON.toJSONString(path));
