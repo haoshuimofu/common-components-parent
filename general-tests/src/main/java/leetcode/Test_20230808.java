581d5c12609b832b2d65f451b6c7adf1ac2835cf
wd <279759300@qq.com>
2023-08-09 00:03:42 +0800

    20230805 leetcode练习

--- general-tests/src/main/java/leetcode/Test_20230808.java
+++ general-tests/src/main/java/leetcode/Test_20230808.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * @author dewu.de
  * @date 2023-08-08 6:07 下午
@@ -9,48 +7,95 @@ import java.util.Arrays;
 public class Test_20230808 {
 
     public static void main(String[] args) {
-        System.out.println(myPow(3, 4));
+//        System.out.println(myPow(1.99364, 11));
+//        System.out.println(myPow(1.72777, 7));
+//        System.out.println(myPow1(1.72777, 7));
+//        System.out.println(myPow(0.00001, 2147483647)); // java.lang.OutOfMemoryError: Requested array size exceeds VM limit
+        System.out.println(myPow1(0.00001, 2147483647));
+
     }
 
+    /**
+     * 剑指 Offer 16. 数值的整数次方
+     * 执行结果 - 超出内存限制
+     *
+     * @param x
+     * @param n
+     * @return
+     */
     public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1; // x的0次幂=1
+        }
         int absN = Math.abs(n);
-        double[] values = new double[absN + 1];
-        values[0] = 1;
-        if (absN > 0) {
+        double[] values = new double[absN];
+        values[0] = x;
+        if (absN > 1) {
             int index = 1;
-            values[1] = x;
-            while (2 * index <= n) {
-                values[2 * index] = values[index] * values[index];
+            while (2 * index <= absN) {
+                values[2 * index - 1] = values[index - 1] * values[index - 1];
                 index *= 2;
+                if (index == absN) {
+                    break;
+                }
             }
-            if (values[n] == 0) {
-                index = index / 2;
-                // 说明 n在区间(index, 2*index)中, 在这个区间找 n-index
-                int low = 0;
-                int high = index;
-                int target = -1;
-                while (low <= high) {
-                    int mid = (low + high) / 2;
-                    if (mid == n - index) {
-                        target = mid;
+            if (values[absN - 1] == 0) {
+                // 跳出循环, 说明2*index > n, n次幂还没求解出, 也说n在区间(index, 2*index)之间
+                int step = index / 2;
+                while (step > 0) {
+                    if (index + step == absN) {
+                        values[absN - 1] = values[index - 1] * values[step - 1];
                         break;
-                    } else if (mid > n - index) {
-                        high = mid - 1;
+                    } else if (index + step > absN) {
+                        step = step / 2;
                     } else {
-                        low = mid + 1;
+                        // index+step -> n-1
+                        values[index + step - 1] = values[index - 1] * values[step - 1];
+                        for (int i = index + step; i < absN; i++) {
+                            values[i] = values[i - 1] * x;
+                        }
+                        break;
                     }
                 }
-                if (values[target] > 0) {
-                    return values[index] * values[target];
-                }
-                // target两边
-                low = target - 1;
-                high = target + 1;
-
-
             }
-
         }
-        return 0;
+        if (n > 0) {
+            return values[absN - 1];
+        } else {
+            return 1 / values[absN - 1];
+        }
     }
+
+    public static double myPow1(double x, int n) {
+        if (n == 0) {
+            return 1; // x的0次幂=1
+        }
+        long absN = Math.abs(n);
+        double value = x;
+        if (absN > 1) {
+            boolean found = false;
+            long index = 1;
+            while (2 * index <= absN) {
+                index *= 2;
+                value = value * value;
+                if (index == absN) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                //  index+1 -> n
+                long subIndex = index / 2;
+                while (index + subIndex <= n) {
+                    value = value + value / 2;
+                    index = index + subIndex / 2;
+                }
+                for (int i = (int) index + 1; i <= n; i++) {
+                    value = value * x;
+                }
+            }
+        }
+        return n > 0 ? value : 1 / value;
+    }
+
 }
