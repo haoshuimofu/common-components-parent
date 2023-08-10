a2c6995cf74215633f8d91c67f91c11c7d8d3f6e
wd <279759300@qq.com>
2023-08-11 00:52:59 +0800

    20230805 leetcode练习

--- general-tests/src/main/java/leetcode/Test_20230810.java
+++ general-tests/src/main/java/leetcode/Test_20230810.java
@@ -3,24 +3,28 @@ package leetcode;
 public class Test_20230810 {
 
     public static void main(String[] args) {
-//        System.out.println(isPowerOfThree(19684));
-        for (int i = 0; i <= 6; i++) {
-            System.out.println(i % 3);
-        }
+        System.out.println(isPowerOfThree(3));
     }
 
     public static boolean isPowerOfThree(int n) {
-        if (n == 0) {
+        if (n < 1) {
+            // 3的x次幂n为整数, 那n肯定>=1
             return false;
         } else if (n == 1) {
+            // 3的0次幂为1
             return true;
         }
-        int sum = 0;
         while (n > 0) {
-            sum += n % 10;
-            n /= 10;
+            if (n % 3 != 0) {
+                // n都不能被3整除, 那n肯定不是3的x次幂
+                return false;
+            }
+            n /= 3;
+            if (n == 1) {
+                return true;
+            }
         }
-        return sum % 3 == 0;
+        return true;
     }
 
 }
