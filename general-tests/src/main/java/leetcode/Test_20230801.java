c731aa15ed4c902266d2c5d6a885a6d8d3a92506
德夏 <dewu.de@alibaba-inc.com>
2023-09-08 17:05:37 +0800

    20230906

--- general-tests/src/main/java/leetcode/Test_20230801.java
+++ general-tests/src/main/java/leetcode/Test_20230801.java
@@ -28,17 +28,23 @@ public class Test_20230801 {
         System.out.println("20 - DFS+回溯耗时: " + (System.currentTimeMillis() - startMillis));
 
         startMillis = System.currentTimeMillis();
-        nums = new int[40];
-        for (int i = 0; i < 40; i++) {
+        nums = new int[30];
+        for (int i = 0; i < 30; i++) {
             nums[i] = i + 1;
         }
         test.sumOfPower(nums);
-        System.out.println("40 - DFS+回溯耗时: " + (System.currentTimeMillis() - startMillis));
+        System.out.println("30 - DFS+回溯耗时: " + (System.currentTimeMillis() - startMillis));
 
     }
 
     /**
      * 2681. 英雄的力量
+     * <p>
+     * 用DFS+回溯, 时间复杂度真是丧心病狂, 当搜索深度和分支因子较大时, 耗时成指数倍上升
+     * 10 - DFS+回溯耗时: 2
+     * 20 - DFS+回溯耗时: 22
+     * 40 - DFS+回溯耗时: 7779590
+     * </p>
      *
      * @param nums
      * @return
