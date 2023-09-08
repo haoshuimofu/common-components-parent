b5aee5b238301e4c31737e752afb17adb6ded883
wd <279759300@qq.com>
2023-09-08 23:30:49 +0800

    20230908今日三题完成

--- general-tests/src/main/java/leetcode/Test_20230908.java
+++ general-tests/src/main/java/leetcode/Test_20230908.java
@@ -8,50 +8,79 @@ public class Test_20230908 {
 
     public static void main(String[] args) {
         Test_20230908 test = new Test_20230908();
-        System.out.println(test.findNthDigit(19));
+        System.out.println(test.findNthDigit(1000000000));
+        System.out.println(test.reverseStr("abcd", 3));
     }
 
-
+    /**
+     * 剑指 Offer 44. 数字序列中某一位的数字
+     * 400. 第 N 位数字
+     *
+     * @param n
+     * @return
+     */
     public int findNthDigit(int n) {
-        int numLen = 0; // 位数
-        int numCount = 1; // 位数长度对应有多少个数字
-        int sumLen = 0;
+        if (n < 10) {
+            return n;
+        }
+        int numLen = 1; // 位数
+        long from = 0;
+        long to = 9;
+        long sumLen = 9; // 序号从0开始, 这里取值-1
 
         while (sumLen < n) {
             numLen++;
-            numCount *= 10;
-            sumLen += numLen * numCount;
+            from = to + 1;
+            to = (long) Math.pow(10, numLen) - 1;
+            sumLen += (to - from + 1) * numLen;
         }
-//        System.out.println(numLen - 1 + " - " + numLen + " - " + sumLen);
         // 循环结束, 说明第n位所在的位置对应的数字是numLen位数
-        // 二分查找, 逼出对应的数字
-        int low = 1;
-        int high = numCount;
-        sumLen -= numLen * numCount;
-//        System.out.println(sumLen);
-        int position = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int addingLen = mid * numLen;
-            if (sumLen + addingLen >= n) {
-                if (sumLen + addingLen - numLen < n) {
-                    System.out.println("OK");
-                    position = mid;
-                    break;
-                } else {
-                    high = mid - 1;
-                }
-            } else {
-                low = mid + 1;
-            }
+        sumLen -= (to - from + 1) * numLen;
+        long index = (n - sumLen) / numLen;
+        if ((n - sumLen) % numLen != 0) {
+            index++;
         }
-        int targetNum = (int) Math.pow(10, numLen - 1) + position - 1;
-//        System.out.println(position + ", target=" + targetNum + ", subLen=" + sumLen);
-        int deltaLen = n - (sumLen + (position - 1) * numLen) + 1;
-//        System.out.println("deltaLen=" + deltaLen);
+        long targetNum = from - 1 + index;
+        long deltaLen = n - sumLen - (targetNum - from) * numLen;
 
         int factor = (int) Math.pow(10, numLen - deltaLen);
-        return (targetNum / Math.max(1, factor)) % 10;
+        return (int) ((targetNum / factor) % 10);
+    }
 
+    /**
+     * 541. 反转字符串 II
+     *
+     * @param s
+     * @param k
+     * @return
+     */
+    public String reverseStr(String s, int k) {
+        int dk = 2 * k;
+        char[] chs = new char[s.length()];
+        int index = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count++;
+            if (count == dk) {
+                for (int j = i - k; j > i - dk; j--) {
+                    chs[index++] = s.charAt(j);
+                }
+                for (int j = i - k + 1; j <= i; j++) {
+                    chs[index++] = s.charAt(j);
+                }
+                count = 0;
+            }
+        }
+        if (count > 0) {
+            int from = s.length() - count;
+            int to = Math.min(from + k, s.length());
+            for (int i = to - 1; i >= from; i--) {
+                chs[index++] = s.charAt(i);
+            }
+            for (int i = to; i < s.length(); i++) {
+                chs[index++] = s.charAt(i);
+            }
+        }
+        return new String(chs, 0, chs.length);
     }
 }
