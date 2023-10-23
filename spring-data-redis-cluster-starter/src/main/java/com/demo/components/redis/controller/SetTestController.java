3394e418a44f7df0bae01fa56b2c927e2e6405a5
德夏 <dewu.de@alibaba-inc.com>
2023-10-23 23:12:30 +0800

    线程中断测试

---
+++ spring-data-redis-cluster-starter/src/main/java/com/demo/components/redis/controller/SetTestController.java
@@ -0,0 +1,54 @@
+package com.demo.components.redis.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.demo.components.JsonResult;
+import com.demo.components.redis.cluster.CacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
+
+/**
+ * @author dewu.de
+ * @date 2023-10-23 7:18 下午
+ */
+@RestController
+@RequestMapping("set/test")
+public class SetTestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SetTestController.class);
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @RequestMapping("score")
+    public JsonResult<Boolean> set() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        String cacheKey = year + "_" + (month + 1) + "_set";
+
+        int maximum = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println(cacheKey + " 有 " + maximum + " 天");
+        int total = 0;
+        for (int i = 0; i < maximum; i++) {
+            String day = String.valueOf(i + 1);
+            long addCnt = cacheManager.getStringRedisTemplate().opsForSet().add(cacheKey, day);
+            total += addCnt;
+        }
+        System.out.print("value total=" + total);
+
+        long cnt = cacheManager.getStringRedisTemplate().opsForSet().add(cacheKey, "10");
+        System.out.println("add 10 to set, result=" + cnt);
+
+        cnt = cacheManager.getStringRedisTemplate().opsForSet().remove(cacheKey, "10");
+        System.out.println("remove 10 from set, result=" + cnt);
+
+
+        return JsonResult.success(true);
+    }
+
+}
